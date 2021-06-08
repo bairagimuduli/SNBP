4d972cf733c11aa25212af80d9460c8007625abc
Bairagi Muduli <bairagi.muduli@mpokket.com>
2021-06-08 21:41:58 +0530

    first commit

---
+++ ApiAutomation_SBPN/src/main/java/com/SBPN/api/postman/Helper.java
@@ -0,0 +1,7 @@
+package com.SBPN.api.postman;
+
+public class Helper {
+    public void test(){
+        System.out.println("Hello World");
+    }
+}
---
+++ ApiAutomation_SBPN/src/test/java/com/SBPN/api/test/TestTest.java
@@ -0,0 +1,14 @@
+package com.SBPN.api.test;
+
+
+import com.SBPN.api.postman.Helper;
+import org.testng.annotations.Test;
+
+public class TestTest {
+
+    @Test
+    public void testName() {
+        Helper helper = new Helper();
+        helper.test();
+    }
+}
